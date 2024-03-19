c8fc229e459379e51b428e82b1be8ebe29403a9b
koutyuke <gunji.kousuke08@gmail.com>
2024-03-19 19:10:53 +0900

    initial: 🎉 initial commit!

---
+++ apps/web/src/app/page.tsx
@@ -0,0 +1,17 @@
+const Page = (): JSX.Element => {
+	return (
+		<main
+			style={{
+				height: "100svh",
+				widows: "100svw",
+				display: "flex",
+				justifyContent: "center",
+				alignItems: "center",
+			}}
+		>
+			<div>hello world</div>
+		</main>
+	);
+};
+
+export default Page;
