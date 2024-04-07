7823eef2dcec8b2226390bb234bc6d571eb3ef0d
koutyuke <gunji.kousuke08@gmail.com>
2024-04-07 11:59:30 +0900

    feat: 🎈 (api/login) ユーザーログインの実装

---
+++ apps/api/src/app/auth/login.ts
@@ -0,0 +1,58 @@
+import { lucia } from "@/src/libs/auth";
+import { prismaClient } from "@/src/libs/prisma";
+import { BadRequestException } from "@/src/plugins/error/exceptions";
+import { logger } from "@/src/plugins/logger";
+import { password as bunPassword } from "bun";
+import { Elysia, t } from "elysia";
+
+const login = new Elysia().use(logger).post(
+	"/login",
+	async ({ body: { email, password }, cookie, set, log }) => {
+		const user = await prismaClient.user.findUnique({
+			where: {
+				email,
+			},
+		});
+
+		if (!user || !user.passwordSalt || !user.hashedPassword) {
+			log.error("User not found.");
+			throw new BadRequestException("User not found.");
+		}
+
+		const passwordPepper = process.env.PASSWORD_PEPPER;
+
+		if (!passwordPepper) {
+			log.error("Password pepper is not set.");
+			throw new Error("Password pepper is not set.");
+		}
+
+		const passwordValid = bunPassword.verify(user.passwordSalt + password + passwordPepper, user.hashedPassword);
+
+		if (!passwordValid) {
+			log.error("Password is invalid.");
+			throw new BadRequestException("Password is invalid.");
+		}
+
+		const session = await lucia.createSession(user.id, {});
+		const sessionCookie = lucia.createSessionCookie(session.id);
+
+		set.status = 200;
+
+		cookie[sessionCookie.name]?.set({
+			value: sessionCookie.value,
+			...sessionCookie.attributes,
+		});
+
+		return user;
+	},
+	{
+		body: t.Object({
+			email: t.String({
+				format: "email",
+			}),
+			password: t.String(),
+		}),
+	},
+);
+
+export { login };
