d1b1234fcf927486e523a895c861a0e6e0004d3c
koutyuke <gunji.kousuke08@gmail.com>
2024-04-07 12:00:27 +0900

    feat: 🎈 (api/login) パスワード認証の登録

--- apps/api/src/app/auth/index.ts
+++ apps/api/src/app/auth/index.ts
@@ -1,7 +1,21 @@
 import { Elysia } from "elysia";
+import { login } from "./login";
+import { logout } from "./logout";
 import { provider } from "./provider";
 import { providerCallback } from "./providerCallback";
+import { signup } from "./signup";
 
-const auth = new Elysia().group("/auth", app => app.use(provider).use(providerCallback));
+const auth = new Elysia({
+	prefix: "/auth",
+})
+	.use(provider)
+	.use(providerCallback)
+	.use(signup)
+	.use(login)
+	.use(logout);
+
+// .group("/auth", app =>
+// 	app.use(provider).use(providerCallback).use(signup).use(login).use(logout),
+// );
 
 export { auth };
